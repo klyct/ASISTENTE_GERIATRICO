82569d83cf4ed2c28a036b65fc90d95b42fd3d9f
klyct <takanashirikka.42@gmail.com>
2023-08-15 19:00:53 -0600

    envia prompt a bard recibe, imprime y lee respuesta --falta hacer excepcion para la presentacion

--- asistente.py
+++ asistente.py
@@ -1,17 +1,31 @@
+from Bard import Chatbot
+token = "Zgg2QUWXYI_Wh9GMuBatHZt6bcpw_tF79eqlj1PQOfoAuQ3VXYe863FItPMWcDSb6_qapA."
+token2 = "APoG2W-kyZETC8vsKtoGT25yYQpZWtVuA-uhZkliilLUChxO02vKlmChXsXSRnxzijyAzAxTgBM"
+chatbot = Chatbot(token, token2)
+
+def prompt_B(prompt):
+    response = chatbot.ask(prompt)
+    return response['content']
+
+
 from listen import Listener
 from talk import Speaker
 def main():
     listener = Listener()
     speaker = Speaker('Kly104', 'JuanitoBananasSeMurio', 'auronplay')
-    try:
-        response = listener.listen()
-        print(response)
-        if 'hola' in response:
-            prompt = response.replace('hola, ','')
-            print(prompt)
-            speaker.talk(f'Este es el promt que se enviara: {prompt}')
-    except Exception as e:
-        print(e)
+    while True:
+        try:
+            response = listener.listen()
+            print(response)
+            if 'hola' in response:
+                prompt = response.replace('hola, ','')
+                print(prompt)
+                answer = prompt_B(prompt)
+                print(answer)
+                speaker.talk(f'{answer}')
+
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     main()
