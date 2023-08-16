fce962558bf204cf56567ec2f2904740ee5c1f47
klyct <takanashirikka.42@gmail.com>
2023-08-15 20:41:11 -0600

    --buscar otro motor para reproducir el audio --prueba con otro api de bard archivo de texto limpio

--- asistente.py
+++ asistente.py
@@ -1,11 +1,16 @@
 from Bard import Chatbot
+
 token = "Zgg2QUWXYI_Wh9GMuBatHZt6bcpw_tF79eqlj1PQOfoAuQ3VXYe863FItPMWcDSb6_qapA."
 token2 = "APoG2W-kyZETC8vsKtoGT25yYQpZWtVuA-uhZkliilLUChxO02vKlmChXsXSRnxzijyAzAxTgBM"
 chatbot = Chatbot(token, token2)
 
+
 def prompt_B(prompt):
-    response = chatbot.ask(prompt)
+    ask = "Actua como un asistente, da una respuesta corta y esto es lo que dijo el usuario: " + prompt
+    response = chatbot.ask(ask)
+
     return response['content']
+
 
 def presentacion():
     pres = "¡Hola! Mi nombre es Kim, soy su asistente geriatrico personal" \
@@ -16,11 +21,21 @@ def presentacion():
            "Puedes verme a traves del prototipo de reproductor que han diseñado para mi" \
            "mis gestos y matices tambien son generados por inteligencia " \
            "artificial para poder brindarte un mejor servicio, eso es todo sobre mi, si tienes alguna duda" \
-           "sobre mi no dudes en preguntarle a mis creadores."##Inserte saludo de presentacion
+           "sobre mi no dudes en preguntarle a mis creadores."
     return pres
+
+
+def cleanResponse(response):
+    caracteres = ["!", "¡", "¿", "?", ",", ".", ";", ":", "-"]
+    for r in caracteres:
+        response = response.replace(r, "")
+    return response
+
 
 from listen import Listener
 from talk import Speaker
+
+
 def main():
     listener = Listener()
     speaker = Speaker('Kly104', 'JuanitoBananasSeMurio', 'auronplay')
@@ -28,20 +43,22 @@ def main():
         try:
             response = listener.listen()
             print(response)
-            if 'kim, presentate' in response:
+            response = cleanResponse(response)
+            if 'hola saludanos' in response:
                 answer = presentacion()
                 print(answer)
-                #speaker.talk(f'{answer}')
-            elif 'kim' in response:
-                prompt = response.replace('kim, ','')
+                # speaker.talk(f'{answer}')
+            elif 'hola' in response:
+                prompt = response.replace('hola ', '')
                 print(prompt)
                 answer = prompt_B(prompt)
                 print(answer)
-                #speaker.talk(f'{answer}')
+                # speaker.talk(f'{answer}')
             else:
                 print("No haz activado al asistente, intenta de nuevo")
         except Exception as e:
             print(e)
+
 
 if __name__ == '__main__':
     main()
--- testBard.py
+++ testBard.py
@@ -1,6 +1,8 @@
-from Bard import Chatbot
-chatbot = Chatbot("Zgg2QUWXYI_Wh9GMuBatHZt6bcpw_tF79eqlj1PQOfoAuQ3VXYe863FItPMWcDSb6_qapA.", "APoG2W-kyZETC8vsKtoGT25yYQpZWtVuA-uhZkliilLUChxO02vKlmChXsXSRnxzijyAzAxTgBM")
-
-answer = chatbot.ask("Hello, how are you?")
-
-print(answer['content'])
+from bardapi import Bard
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv("Zgg2QUWXYI_Wh9GMuBatHZt6bcpw_tF79eqlj1PQOfoAuQ3VXYe863FItPMWcDSb6_qapA.")
+bard = Bard(token=token)
+response = bard.get_answer('saludame')
+print(response['content'])
